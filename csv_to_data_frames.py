c753931d76c2e44143aa22e8e2ed0501edbac12c
Panyi <adam.panyiczki@edreamsodigeo.com>
2022-07-07 11:48:13 +0200

    handling_invalid_column_names

--- csv_to_data_frames.py
+++ csv_to_data_frames.py
@@ -1,3 +1,4 @@
+from pickle import APPEND, TRUE
 import pandas as pd
 import os
 from pathlib import Path
@@ -6,30 +7,36 @@ import pandas_gbq
  
 
 def finding_and_uploading_files():
-    directory = 'baseballdatabank-2022.2/contrib/'
+    lst_of_folders = ["contrib","core","upstream"]
+    for folder in lst_of_folders:
+        directory = f'baseballdatabank-2022.2/{folder}/'
 
-    # iterate over files in
-    # that directory
-    for filename in os.scandir(directory):
-        if filename.is_file():
-            files_path= (str(filename.path))    
-            df = pd.read_csv(files_path)
+        # iterate over files in
+        # that directory
+        for filename in os.scandir(directory):
+            if filename.is_file():
+                files_path= (str(filename.path))    
+                df = pd.read_csv(files_path)
 
-            # file name with extension
-            file_name = os.path.basename(files_path)
+                # handling if there is an invalid column name for table creaton             
+                df.columns = '_'+df.columns
+                print(df.columns)
+                    
+                # file name with extension
+                file_name = os.path.basename(files_path)
 
-            # file name without extension
-            file_name = (os.path.splitext(file_name)[0])
+                # file name without extension
+                file_name = (os.path.splitext(file_name)[0])
 
-            # Set project_id to your Google Cloud Platform project ID.
-            project_id = "panyi-sabermetrics"
+                # Set project_id to your Google Cloud Platform project ID.
+                project_id = "panyi-sabermetrics"
 
-            # Set table_id to the full destination table ID (including the dataset ID).
-            table_id = f"panyi-sabermetrics.mlb_data.{file_name}"
+                # Set table_id to the full destination table ID (including the dataset ID).
+                table_id = f"panyi-sabermetrics.mlb_data.{folder}_{file_name}"
+                pandas_gbq.to_gbq(df, table_id, project_id=project_id, if_exists="append")
 
-            pandas_gbq.to_gbq(df, table_id, project_id=project_id)
-
-       
+               
+                       
 
 if __name__ == "__main__":
     finding_and_uploading_files()
