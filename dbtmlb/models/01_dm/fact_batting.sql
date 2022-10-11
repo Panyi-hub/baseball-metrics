4dfaa8e769f41b14777ee47e27b84a0153ae497a
Panyi <adam.panyiczki@edreamsodigeo.com>
2022-10-11 18:01:08 +0200

    fact_batting_refactored

--- dbtmlb/models/01_dm/fact_batting.sql
+++ dbtmlb/models/01_dm/fact_batting.sql
@@ -1,3 +1,16 @@
 SELECT
-    *
+      _playerID AS people_key
+    , _teamID AS team_key
+    , _yearID AS season_key
+    , _G AS games_played
+    , _AB AS at_bat
+    , _R AS run
+    , _H AS hit
+    , _2B AS double_base_hit
+    , _3B AS triple_base_hit
+    , _HR AS home_runs
+    , _RBI AS run_batted_in
+    , _SB AS stolen_bases
+    , _BB AS base_walking
+    , _SO AS strike_outs
 FROM {{ source('mlb_data', 'core_Batting') }}
