ad785bef39986a6e8b00e80d19bd37fe7da71d1e
Panyi <adam.panyiczki@edreamsodigeo.com>
2022-10-12 10:52:47 +0200

    refactoring_dim_awards

--- dbtmlb/models/01_dm/dim_awards.sql
+++ dbtmlb/models/01_dm/dim_awards.sql
@@ -1,5 +1,15 @@
 SELECT
-      b._awardID AS awards_key
-    , b._playerID AS people_key
-    , b._yearID AS year
-FROM {{ source('mlb_data', 'contrib_AwardsManagers') }} b
+      m._awardID AS awards_key
+    , m._playerID AS people_key
+    , m._yearID AS year
+    , NULL AS league
+FROM {{ source('mlb_data', 'contrib_AwardsManagers') }} m
+
+UNION ALL
+
+SELECT
+      p._awardID AS awards_key
+    , p._playerID AS people_key
+    , p._yearID AS year
+    , p._lgID AS league
+FROM {{ source('mlb_data', 'contrib_AwardsPlayers') }} p
