0f7ff9e862fb1c06c6fd88db7828ab73aed47660
Panyi <adam.panyiczki@edreamsodigeo.com>
2022-10-18 17:28:27 +0200

    extending_fact_table_with_all_the_keys

--- dbtmlb/models/01_dm/dim_teams.sql
+++ dbtmlb/models/01_dm/dim_teams.sql
@@ -1,6 +1,7 @@
 
 SELECT 
-      distinct ct._franchID AS team_key
+      CONCAT(ct._franchID,"-",ct._yearID) AS team_key
+    , ct._yearID AS season_key
     , ct._name AS franchise_name
     , cp._park_name AS ballpark
     , cp._city as team_city
@@ -8,5 +9,4 @@ SELECT
     , cp._country AS team_country
 FROM {{ source('mlb_data', 'upstream_Teams') }} ct
 LEFT JOIN {{ source('mlb_data', 'core_Parks') }} cp ON ct._park = COALESCE(cp._park_alias, cp._park_name)
-WHERE cp._park_name IS NOT NULL
-ORDER BY 2 ASC
+ORDER BY 1,2,3
--- dbtmlb/models/01_dm/fact_batting.sql
+++ dbtmlb/models/01_dm/fact_batting.sql
@@ -1,16 +1,20 @@
 SELECT
-      _playerID AS people_key
-    , _teamID AS team_key
-    , _yearID AS season_key
-    , _G AS games_played
-    , _AB AS at_bat
-    , _R AS run
-    , _H AS hit
-    , _2B AS double_base_hit
-    , _3B AS triple_base_hit
-    , _HR AS home_runs
-    , _RBI AS run_batted_in
-    , _SB AS stolen_bases
-    , _BB AS base_walking
-    , _SO AS strike_outs
-FROM {{ source('mlb_data', 'core_Batting') }}
+      cb._playerID AS people_key
+    , CONCAT(cb._teamID,"-",cb._yearID) AS team_key
+    , cb._yearID AS season_key
+    , ccp._schoolID AS school_key
+    , cap._awardID AS awards_key
+    , cb._G AS games_played
+    , cb._AB AS at_bat
+    , cb._R AS run
+    , cb._H AS hit
+    , cb._2B AS double_base_hit
+    , cb._3B AS triple_base_hit
+    , cb._HR AS home_runs
+    , cb._RBI AS run_batted_in
+    , cb._SB AS stolen_bases
+    , cb._BB AS base_walking
+    , cb._SO AS strike_outs
+FROM {{ source('mlb_data', 'core_Batting') }} cb
+LEFT JOIN {{ source('mlb_data', 'contrib_CollegePlaying') }} ccp ON cb._playerID = ccp._playerID
+LEFT JOIN {{ source('mlb_data', 'contrib_AwardsPlayers') }} cap ON cb._playerID = cap._playerID
